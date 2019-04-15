cbaf5d874b98ea41bad3d60e0f1e6fdb305eb6a7
hejernst <ernst.cederholm@gmail.com>
2019-04-15 15:51:45 +0200

    byt till min stack

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,38 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String args[])
+    {
+        // Creating an empty Stack
+        Stack stack = new Stack();
+        System.out.println("Input integer or operator");
+        while(true) {
+            Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+            String input = sc.nextLine();
+            if(input.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            }
+            else if(input.equals("-")) {
+                stack.push((-1)*stack.pop() + stack.pop());
+            }
+            else if(input.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            }
+            else if(input.equals("/")) {
+                stack.push((1/stack.pop()) * stack.pop());
+            }
+            else if(input.equals("=")) {
+                break;
+            }
+            else {
+                stack.push(Double.valueOf(input));
+            }
 
-        Stack hej = new Stack();
-        hej.push(5);
-        System.out.println(hej.pop());
-        System.out.println(Arrays.toString(hej.getStack()));
-        //double[] a = new double[10];
-        //System.out.println(Arrays.toString(a));
+        }
+        double answer;
+        answer = stack.pop();
+        System.out.println(answer);
+
     }
 }
